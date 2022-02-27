c508121517b768b642444b1d8650932f254ab502
JeroenMBooij <49719495+JeroenMBooij@users.noreply.github.com>
2022-02-27 12:35:05 +0100

    url

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portal'`, () => {
+  it(`should have as title 'Portal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portal');
+    expect(app.title).toEqual('Portal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('portal app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Portal app is running!');
   });
 });
